198acb02568318069091fdad048d478f9f50efeb
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-11-27 09:07:39 +0530

    Day 03

--- Day 02/daytwoapp/src/App.js
+++ Day 02/daytwoapp/src/App.js
@@ -1,26 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About This Page</h1>
+      <p>Hello there.<br />i am the who is writing all of these nonsence</p>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         
+        <h1>Now it's Under Control </h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload hi i Build this my Own.
+          Yah! it's Normal css
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h2>Hello How are You me whose the bose</h2>
+        <button>Submit</button>
       </header>
     </div>
   );
 }
 
-export default App;
+// export default App;
+export default AboutPage;
